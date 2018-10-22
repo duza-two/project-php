898fbb7cdd7ad612be074215a6ead9249527b736
Duane <d_sm9@hotmail.com>
2018-10-22 17:37:03 +0200

    test fix

--- email_form.php
+++ email_form.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submit'])){
-    $to = "d_sm9@hotmail.com"; // this is your Email address
+    $to = "candicesm@hollard.co.za"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['name'];
     $subject = "Form submission";
@@ -8,9 +8,7 @@ if(isset($_POST['submit'])){
     $message = $first_name . " wrote the following:" . "\n\n" . $_POST['comment'];
     $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['comment'];
 
-    $headers  = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= 'From:' . $from . "\r\n";
+    $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
